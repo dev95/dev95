b0694870808421086085871175d573899057a91c
ditsing <ditsing@gmail.com>
2012-04-08 14:55:19 +0800

    Rewrite deploy script

--- cgi/deploy.sh
+++ cgi/deploy.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-rm -r /usr/lib/cgi-bin/dev95
-mv /var/www/dev95/cgi /usr/lib/cgi-bin/dev95
+
+REPO_PATH=~
+DIR_NAME="jwc2ical"
+if [ -n "$1"]; then
+	if [ "$1" -eq "test"]; then
+		$DIR_NAME = "dev95";
+	else
+		echo "Not recognized para $1\n";
+		exit 1;
+	fi
+fi
+
+rm /usr/lib/cgi-bin/$DIR_NAME/* -rf
+rm /var/www/$DIR_NAME/* -rf
+
+cp $REPO_PATH/$DIR_NAME/* /var/www/$DIR_NAME/ -R
+mv /var/www/$DIR_NAME/cgi/* /usr/lib/cgi-bin/$DIR_NAME/ -R
+rmdir /var/www/$DIR_NAME/cgi
